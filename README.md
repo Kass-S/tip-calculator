3893ea41a0af01eb12a727825fd7956c5174de50
Kass-S <kassiespacek101@gmail.com>
2025-03-10 10:24:12 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Kassandra Spacek
+3/10/25
+
+Create a tip Calculator from the FrontEndMentor starter
+
+Reviewed by:
---
+++ src/components/TipCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TipCard = () => {
+  return (
+    <div>TipCard</div>
+  )
+}
+
+export default TipCard
