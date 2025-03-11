5c2f9c0a8a4bbb0a4a0abac1e6989751084fa493
Kass-S <kassiespacek101@gmail.com>
2025-03-11 15:27:13 -0700

    updated style

--- src/components/TipCard.jsx
+++ src/components/TipCard.jsx
@@ -1,4 +1,3 @@
-import { Card } from 'flowbite-react'
 import React from 'react'
 
 const TipCard = () => {
@@ -12,7 +11,7 @@ const TipCard = () => {
                         Bill
                     </h5>
                     <img src="/assets/icon-dollar.svg" alt="" />
-                    <input type="number" className='input-bg rounded-lg text-right' />
+                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color' />
                 </div>
                 
                 <div className='my-6'>
@@ -25,7 +24,7 @@ const TipCard = () => {
                         <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>15%</button>
                         <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>25%</button>
                         <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>50%</button>
-                        <input type="text" placeholder="Custom" className='input-bg rounded-md text-lg py-1 m-2 text-right' /> 
+                        <input type="text" placeholder="Custom" className='input-bg rounded-md text-lg py-1 m-2 text-right input-text-color' /> 
                     </div>
                 </div>
 
@@ -33,13 +32,13 @@ const TipCard = () => {
                     <h5 className=" font-bold text-color-dark">
                         Number of People
                     </h5>
-                    <input type="number" className='input-bg rounded-lg text-right' />
+                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color' />
                 </div>
                 
             </div>      
             
-            <div className="max-w-lg inner-card-bg pl-8 p-4 m-6 rounded-2xl">
-                <div className='my-6'>
+            <div className="max-w-lg inner-card-bg p-4 m-6 rounded-2xl">
+                <div className='m-6'>
                     <h5 className="font-bold text-white">
                         Tip Amount
                     </h5>
@@ -48,7 +47,7 @@ const TipCard = () => {
                     </p>
                 </div>
                 
-                <div className='my-6'>
+                <div className='m-6'>
                     <h5 className="font-bold text-white">
                         Total
                     </h5>
@@ -57,9 +56,12 @@ const TipCard = () => {
                     </p>
                 </div>
 
-                <button className='text-white button-hover rounded-lg reset-btn-bg p-2 px-6'>Reset</button>
-                
+                <div className='m-6 mt-16'>
+                    <button className='text-white button-hover rounded-lg reset-btn-bg p-2 w-full '>Reset</button>
+                </div>
+
             </div>
+
         </div>
     </div>
   )
