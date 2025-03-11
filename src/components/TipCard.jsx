5deba42716949429d7012b6c8116cf7997b6b5de
Kass-S <kassiespacek101@gmail.com>
2025-03-11 09:32:00 -0700

    added card

--- src/components/TipCard.jsx
+++ src/components/TipCard.jsx
@@ -1,8 +1,19 @@
+import { Card } from 'flowbite-react'
 import React from 'react'
 
 const TipCard = () => {
+
   return (
-    <div>TipCard</div>
+    <div className='flex justify-center'>
+        <Card className="max-w-4xl min-w-52">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+        </Card>
+    </div>
   )
 }
 
