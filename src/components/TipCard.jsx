d7bd2d1f8f2d1b1729f3d9b5be03072262a9faf3
Kass-S <kassiespacek101@gmail.com>
2025-03-11 15:00:11 -0700

    style updates

--- src/components/TipCard.jsx
+++ src/components/TipCard.jsx
@@ -6,18 +6,35 @@ const TipCard = () => {
   return (
     <div className='flex justify-center'>
         <div className="lg:w-[56rem] grid grid-cols-2 bg-white rounded-2xl lg:h-96">
-            <div className='m-6 max-w-lg'>
-                <h5 className=" font-bold text-color-dark">
-                    Bill
-                </h5>
-
-                <h5 className=" font-bold text-color-dark">
-                    Select Tip %
-                </h5>
+            <div className=' max-w-lg'>
+                <div className='m-6 mt-8'>
+                    <h5 className=" font-bold text-color-dark">
+                        Bill
+                    </h5>
+                    <input type="number" className='input-bg rounded-lg' />
+                </div>
                 
-                <h5 className=" font-bold text-color-dark">
-                    Number of People
-                </h5>
+                <div className='my-6'>
+                    <h5 className=" font-bold text-color-dark mx-6">
+                        Select Tip %
+                    </h5>
+                    <div className='grid grid-cols-3 mx-4'>
+                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>5%</button>
+                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>10%</button>
+                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>15%</button>
+                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>25%</button>
+                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>50%</button>
+                        <input type="text" placeholder="Custom" className='input-bg rounded-md text-lg py-1 m-2' />
+                    </div>
+                </div>
+
+                <div className='m-6 mb-2'>
+                    <h5 className=" font-bold text-color-dark">
+                        Number of People
+                    </h5>
+                    <input type="number" className='input-bg rounded-lg' />
+                </div>
+                
             </div>      
             
             <div className="max-w-lg inner-card-bg pl-8 p-4 m-6 rounded-2xl">
@@ -38,6 +55,8 @@ const TipCard = () => {
                         / person
                     </p>
                 </div>
+
+                <button className='text-white button-hover rounded-lg reset-btn-bg'>Reset</button>
                 
             </div>
         </div>
