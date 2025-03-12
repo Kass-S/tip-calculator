c6eb00c57d745b3d395ab594a8d882496d1aace2
Kass-S <kassiespacek101@gmail.com>
2025-03-12 10:30:29 -0700

    style updates

--- src/components/TipCard.jsx
+++ src/components/TipCard.jsx
@@ -4,27 +4,27 @@ const TipCard = () => {
 
   return (
     <div className='flex justify-center'>
-        <div className="lg:w-[56rem] grid grid-cols-2 bg-white rounded-2xl lg:h-96">
+        <div className="lg:w-[56rem] grid lg:grid-cols-2 bg-white rounded-2xl lg:h-96">
             <div className=' max-w-lg'>
                 <div className='m-6 mt-8'>
                     <h5 className=" font-bold text-color-dark">
                         Bill
                     </h5>
                     {/* <img src="/assets/icon-dollar.svg" alt="dollar sign icon" /> */}
-                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color' />
+                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color font-bold text-xl' />
                 </div>
                 
                 <div className='my-6'>
                     <h5 className=" font-bold text-color-dark mx-6">
                         Select Tip %
                     </h5>
-                    <div className='grid grid-cols-3 mx-4'>
-                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>5%</button>
-                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>10%</button>
-                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>15%</button>
-                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>25%</button>
-                        <button className='inner-card-bg rounded-md text-lg py-1 m-2 text-white button-hover'>50%</button>
-                        <input type="text" placeholder="Custom" className='input-bg rounded-md text-lg py-1 m-2 text-right input-text-color' /> 
+                    <div className='grid grid-cols-2 md:grid-cols-3 mx-4 font-bold'>
+                        <button className='inner-card-bg rounded-md text-xl py-2 m-2 text-white button-hover'>5%</button>
+                        <button className='inner-card-bg rounded-md text-xl py-2 m-2 text-white button-hover'>10%</button>
+                        <button className='inner-card-bg rounded-md text-xl py-2 m-2 text-white button-hover'>15%</button>
+                        <button className='inner-card-bg rounded-md text-xl py-2 m-2 text-white button-hover'>25%</button>
+                        <button className='inner-card-bg rounded-md text-xl py-2 m-2 text-white button-hover'>50%</button>
+                        <input type="text" placeholder="Custom" className='input-bg rounded-md text-xl py-2 m-2 text-right input-text-color font-bold' /> 
                     </div>
                 </div>
 
@@ -32,13 +32,13 @@ const TipCard = () => {
                     <h5 className=" font-bold text-color-dark">
                         Number of People
                     </h5>
-                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color' />
+                    <input type="number" className='input-bg rounded-lg text-right w-full input-text-color font-bold text-xl' />
                 </div>
                 
             </div>      
             
             <div className="max-w-lg inner-card-bg p-4 m-6 rounded-2xl">
-                <div className='m-6 flex justify-between'>
+                <div className='m-2 my-3 md:m-6 flex justify-between'>
                     <div>
                         <h5 className="font-bold text-white">
                             Tip Amount
@@ -51,7 +51,7 @@ const TipCard = () => {
                     <p className='money-number-color text-3xl font-bold'>$0.00</p>
                 </div>
                 
-                <div className='m-6 flex justify-between'>
+                <div className='m-2 my-3 md:m-6 flex justify-between'>
                     <div>
                         <h5 className="font-bold text-white">
                             Total
@@ -64,8 +64,8 @@ const TipCard = () => {
                     <p className='money-number-color text-3xl font-bold'>$0.00</p>
                 </div>
 
-                <div className='mx-6 mt-28'>
-                    <button className='text-white button-hover rounded-lg reset-btn-bg p-2 w-full '>Reset</button>
+                <div className='mx-2 md:mx-6 mt-6 md:mt-28'>
+                    <button className='input-text-color button-hover rounded-lg reset-btn-bg p-2 w-full font-bold'>RESET</button>
                 </div>
 
             </div>
